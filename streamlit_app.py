543ba65dbe52897bed5fb42ce1c862b456f16452
wolfpack se kwon <62969530+by-sekwon@users.noreply.github.com>
2025-07-19 09:37:28 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ import pandas as pd
 
 # 수동 종목 리스트 정의
 data = {
-    "Name": ["삼성전자", "네이버", "카카오"],
-    "Symbol": ["005930", "035420", "035720"]
+    "Name": ["삼성전자", "네이버", "카카오", "현대자동차", "SK텔레콤"],
+    "Symbol": ["005930", "035420", "035720", "005380", "017670"]
 }
 df_krx = pd.DataFrame(data)
 
@@ -16,7 +16,7 @@ selected_name = st.selectbox("종목 선택", df_krx["Name"])
 selected_code = df_krx[df_krx["Name"] == selected_name]["Symbol"].values[0]
 
 # 주가 데이터
-start = datetime.today() - timedelta(days=30)
+start = datetime.today() - timedelta(days=90)
 df_price = fdr.DataReader(selected_code, start)
 
 st.subheader(f"{selected_name}의 최근 30일 종가")
